390a9900aa1a38591cecf086dc2db10c979507ea
Pavel Lazureykis <pavel@lazureykis.dev>
2022-08-21 12:20:50 -0400

    example 2: read state

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,38 @@
 use std::env::args;
+use std::thread::sleep;
+use std::time::Duration;
 
+mod counter;
 mod simple_counter;
 
 fn main() {
     let command = args()
         .nth(1)
         .expect("USAGE: rust_gen_server_example simple_counter");
+
     match command.as_str() {
         "simple_counter" => {
-            let (sender, join_handle) = simple_counter::start_counter();
+            let (sender, _join_handle) = simple_counter::start_counter();
 
             sender.send(1).unwrap();
             sender.send(5).unwrap();
             sender.send(1).unwrap();
 
-            join_handle.join().unwrap();
+            sleep(Duration::from_millis(100));
+            // join_handle.join().unwrap();
+        }
+        "counter" => {
+            let (counter, _join_handle) = counter::Counter::start();
+
+            counter.increment(1);
+            counter.increment(5);
+            counter.increment(1);
+
+            let current_value = counter.value();
+            println!("Current counter value is {}", &current_value);
+
+            sleep(Duration::from_millis(100));
+            // join_handle.join().unwrap();
         }
         _ => println!("Unknown command"),
     }
